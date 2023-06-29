c4e7ae7137bfd4523ace1288d89c928cfca01694
Slashure <125664846+Slashure@users.noreply.github.com>
2023-06-29 13:33:18 +0200

    Update gkey.py

--- gkey.py
+++ gkey.py
@@ -1,8 +1,8 @@
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 get_keys functie
-Auteur: Kasper van der Mark
-Datum: 29-03-2023
-Versie: 1.0
+Auteur: Victor Liu
+Datum: 29-06-2023
+Versie: 3.0
 
 Dit programma is een kleine functie die keys ophaalt uit een bestand.
 de functie is bedoeld om in een groter programma gebruikt te worden,
@@ -14,17 +14,20 @@ Voor veilig gebruik van dit Python-script dienen de sleutels te worden aangepast
 Ik aanvaard geen enkele verantwoordelijkheid voor eventuele schade die kan ontstaan door het gebruik van dit script.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 from time import sleep
+import subprocess
 
 def main():
     print('dit is de gkey functie script')
-    file_path = input('wat is het pad naar de keys: ')
-    #file_path = ('/etc/wireguard')
+    keygen()
+
+
+    #file_path = input('wat is het pad naar de keys: ')
+    file_path = ('/home/vicc')
     print('Dit is de path', file_path)
-    client, server, test = gkey(file_path) 
+    client, server, = gkey(file_path) 
     print("client: ", client)
     print("server: ", server)
-    print("test", test)
-    print("test gkey," gkey)
+    print("test gkey", gkey)
 
 def gkey(file_path):
     """
@@ -34,20 +37,38 @@ def gkey(file_path):
     """
     while True:
         try:
-            key_file_path = file_path + '/wgclient,key' #het is van belang dat je van meerdere variabelen 1 variabel maakt
+            key_file_path = file_path + '/publickey' #het is van belang dat je van meerdere variabelen 1 variabel maakt
             print(key_file_path)
-            with open(file_path + '/wgclient.key', 'r') as file:
+            with open(file_path + '/publickey', 'r') as file:
                 client = file.read()
-            with open(file_path + '/wgserver.pub', 'r') as file:
+            with open(file_path + '/privatekey', 'r') as file:
                 server = file.read()
-            with open(file_path + '/test.txt','r') as file: 
-                test = file.read()
-            return client, server, test
+            return client, server
             print(test)
         except FileNotFoundError as error:
             print(error)
             print("Sleutel niet gevonden. Opnieuw proberen in 5 seconden...")
             sleep(5)
+
+def keygen():
+    Ans = input('Wil je nieuwe keys genereren? (Y/N): ')
+    if Ans in ['y', 'Y']:
+        print ("Keygen start")
+        print("pizza")
+
+        subprocess.run(['sudo', 'rm', '/home/vicc/publickey'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['sudo', 'rm', '/home/vicc/privatekey'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        privatekey = subprocess.check_output("wg genkey", shell=True).decode("utf-8").strip()
+        publickey = subprocess.check_output(f"echo '{privatekey}' | wg pubkey", shell=True).decode("utf-8").strip()
+        print(publickey)
+        print(privatekey)
+        with open('/home/vicc/publickey', 'w') as f:
+            f.write(publickey)
+        with open('/home/vicc/privatekey', 'w') as f:
+            f.write(privatekey)
+    else:
+        print("Abort Keygen")
+
 
 def test():
     """
