0f579a71656ef65df68e44ed3d071a52bf739837
Slashure <125664846+Slashure@users.noreply.github.com>
2023-07-07 15:03:52 +0200

    Update wconf.py

--- wconf.py
+++ wconf.py
@@ -1,14 +1,21 @@
-""""
+""""""""""
 Wcon.py
 Wireguard config
 Auteur: Victor Liu
-Datum: 30-03-2023
-Versie: 5.0
+Datum: 07-07-2023
+Versie: 8.0 eind
 
+
+Vereisten:
+pip install python-wireguard
+
+Documentatie:
+Wireguard-pakket: https://pypi.org/project/python-wireguard/
+Wireguard:        https://www.wireguard.com/embedding/
 Een script dat automatisch een configuratie maakt. Het is wel van belang dat de gkey.py bestand erbij gebruikt moet worden om keys te generen en in de config te doen
 
-""""
-#modules
+"""""""""
+# Importeer vereiste modules
 from python_wireguard import Client, ServerConnection, Key
 from gkey import gkey
 import subprocess
@@ -27,7 +34,9 @@ server_key = Key(server_key_str.strip())
 endpoint = "80.80.80.2"
 port = 51820
 
-subprocess.run(['sudo', 'wg-quick', 'down', 'wg0'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) #stop huidige wireguard interface
+
+# Stop en verwijder bestaande WireGuard-configuratie
+subprocess.run(['sudo', 'wg-quick', 'down', 'wg0'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  #stop huidige wireguard interface
 subprocess.run(['sudo', 'rm', '/etc/wireguard/wg0.conf'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) #Verwijder config file
 
 # Maak WireGuard-client- en server-verbindingobjecten
@@ -35,11 +44,12 @@ client = Client('wg0', client_key, local_ip)
 server_conn = ServerConnection(server_key, endpoint, port)
 client.set_server(server_conn)
 
-# Maak een configuratiebestand
+# Maak configuratiebestand
 config = f'''[Interface]
 PrivateKey = {client_key}
 ListenPort = {listen_port}
 Address = {local_ip}
+
 
 [Peer]
 PublicKey = {server_key}
@@ -47,7 +57,7 @@ AllowedIPs = 10.10.10.0/24, 192.168.0.0/16
 Endpoint = {endpoint}:{port}
 '''
 
-# Schrijf een configuratiebestand
+# Schrijf configuratiebestand
 with open('/etc/wireguard/wg0.conf', 'w') as f:
     f.write(config)
 
